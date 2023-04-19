d822f3b7bcef1930bcf03a9e08622b0095f0b793
AustinRado <radoaussy@gmail.com>
2023-04-19 11:45:50 -0700

    initial commit

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#endif
