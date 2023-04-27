deed23104f82c3142201539c71197a48baafda8c
AustinRado <radoaussy@gmail.com>
2023-04-27 00:31:06 -0700

    changes

--- path_finder.c
+++ path_finder.c
@@ -3,26 +3,41 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_PATH_LENGTH 4096
+#define MAX_PATH_LENGTH 1024
+#define MAX_FILENAME_LENGTH 256
+#define MAX_COMMAND_LENGTH (MAX_PATH_LENGTH + MAX_FILENAME_LENGTH + 1)
 
 char *find_path(char *command)
 {
 	char *path = getenv("PATH");
-	char *dir = strtok(path, ":");
-	char *full_path = (char *) malloc(MAX_PATH_LENGTH * sizeof(char));
+	char *token;
+	char *path_component;
+	char *command_path = (char*) malloc(MAX_COMMAND_LENGTH);
+	
 
-	while (dir != NULL)
+	if (path == NULL)
 	{
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-
-		if (access(full_path, X_OK) == 0)
-		{
-			return (full_path);
-		}
-		dir = strtok(NULL, ":");
+		fprintf(stderr, "Error: PATH environment variable not set.\n");
+		return NULL;
 	}
-	free(full_path);
-	return (NULL);
+	token = strtok(path, ":");
+
+	while (token != NULL)
+	{
+		path_component = (char*) malloc(strlen(token) + 2 + strlen(command));
+		sprintf(path_component, "%s/%s", token, command);
+
+		if ((access(path_component, X_OK) == 0))
+		{
+			strncpy(command_path, path_component, MAX_COMMAND_LENGTH);
+			free(path_component);
+			return command_path;
+		}
+
+		free(path_component);
+		token = strtok(NULL, ":");
+	}
+	free(command_path);
+	
+	return(NULL);
 }
